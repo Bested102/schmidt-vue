424560475edf4e770e44fc1748bdbeb8e2053064
Omar <omar4444yasser@gmail.com>
2024-08-19 20:40:56 +0300

    hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import BlogView from "@/views/BlogView.vue";
@@ -8,9 +8,10 @@ import experiencesView from "@/views/ExperiencesView.vue";
 import ServicesView from "@/views/ServicesView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      
       path: "/",
       name: "Home",
       component: HomeView,
